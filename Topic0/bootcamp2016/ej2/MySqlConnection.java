d95c47d27a29fc24ae5b835386f337b12154981a
Braian Varona <brvarona@gmail.com>
2016-01-13 10:38:52 -0300

    Agrego codigo fuente correspondiente a los ejercicios 1 y 2 de la guia practica

---
+++ Topic0/bootcamp2016/ej2/MySqlConnection.java
@@ -0,0 +1,11 @@
+package bootcamp2016.ej2;
+
+public class MySqlConnection implements Connection {
+
+	@Override
+	public void connect() {
+		System.out.println("Connecting to MySQL");
+		// ...
+	}
+
+}
