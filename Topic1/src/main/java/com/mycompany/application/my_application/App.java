5fd948485a4613d16226c8f08abee486d1a3d9d3
Braian Varona <brvarona@gmail.com>
2016-01-19 04:34:29 -0300

    Adding the exercises of Topic1

---
+++ Topic1/src/main/java/com/mycompany/application/my_application/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.application.my_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(getHelloWorld());
+    }
+    
+    public static String getHelloWorld(){
+    	return "Hello World!";
+    }
+}
