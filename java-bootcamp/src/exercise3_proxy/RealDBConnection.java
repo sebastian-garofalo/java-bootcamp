739662f715d4341c03e05ff91a6b59d61d68da4f
Sergio <arrietasergio94@gmail.com>
2018-07-03 17:03:43 -0300

    Added RealSubject from proxy pattern

---
+++ java-bootcamp/src/exercise3_proxy/RealDBConnection.java
@@ -0,0 +1,43 @@
+package exercise3_proxy;
+import exercise1_singleton.DBdummy;
+
+public class RealDBConnection implements DBInterface{
+	
+	private String user;
+	private String pass;
+		
+	@Override
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	@Override
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	@Override
+	public String getUser() {
+		return user;
+	}
+	
+	@Override
+	public String getPass() {
+		return pass;
+	}
+	
+	@Override
+	public String query(String request) {
+		return DBdummy.query(request);
+	}
+	
+	@Override
+	public boolean connect() {
+		return DBdummy.login(user, pass);
+	}
+
+	@Override
+	public boolean closeSession() {
+		return DBdummy.closeSession();
+	}
+}
