0973966eee1025f03f10001794007c96f530148e
Simon <Simon@192.168.0.83>
2018-07-05 17:45:56 -0300

    Singleton Topic 0

--- Topic 0/src/Singleton.java
+++ Topic 0/src/Singleton.java
@@ -1,4 +1,36 @@
-
+import java.sql.*;
 public class Singleton {
-
+	// Properties
+    private static Connection conn = null;
+    private String driver;
+    private String host;
+    private String user;
+    private String password;
+ 
+    // Builder
+    private Singleton(){
+ 
+ String host = "jdbc:mysql://localhost:3306/test";
+ String driver = "com.mysql.jdbc.Driver";
+ String user = "user";
+ String password = "password";
+  
+ try{
+     Class.forName(driver);
+     conn = DriverManager.getConnection(host, user, password);
+ }
+ catch(ClassNotFoundException | SQLException e){
+     e.printStackTrace();
+ }
+    } // End Buldier
+ 
+    // Methods
+    public static Connection getConnection(){
+  
+ if (conn == null){
+     new Singleton();
+ }
+  
+ return conn;
+    } // End getConnection
 }
