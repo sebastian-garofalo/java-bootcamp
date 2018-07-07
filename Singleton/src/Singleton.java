86e881d7a74b6deee7f77cd4b44ceb14ff9336e7
Simon <Simon@192.168.0.83>
2018-07-07 11:11:01 -0300

    Topic 0: Introduction to Object Oriented Programming Concepts

--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -1,4 +1,37 @@
-
+import java.sql.*;
 public class Singleton {
-
+	  // Properties
+    private static Connection conn = null;
+    private String driver;
+    private String host;
+    private String user;
+    private String password;
+ 
+    // Constructor
+    private Singleton(){
+ 
+ String host = "jdbc:mysql://localhost:3306/test";
+ String driver = "com.mysql.jdbc.Driver";
+ String user = "user";
+ String password = "password";
+  
+ try{
+     Class.forName(driver);
+     conn = DriverManager.getConnection(host, user, password);
+ }
+ catch(ClassNotFoundException | SQLException e){
+     e.printStackTrace();
+ }
+    } // End constructor
+ 
+    // Methods
+    public static Connection getConnection(){
+  
+ if (conn == null){
+     new Singleton();
+ }
+  
+ return conn;
+    } // End getConnection
+}
 }
